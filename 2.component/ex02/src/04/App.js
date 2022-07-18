ceeaea3a3432f440062d24c03df3a3b5e164f3bb
hwimin-kim <gnlals7841@gmail.com>
2022-07-18 10:18:49 +0900

    component styling #3

---
+++ 2.component/ex02/src/04/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Banner01 from './Banner01';
+import Banner02 from './Banner02';
+import styles from './assets/css/App.css';
+
+function App() {
+    return (
+        <div id='App'>
+            <h1>CSS Module II</h1>
+            <Banner01 />
+            <Banner02 />
+        </div>
+    );
+}
+
+export default App
