a1889ce9e8621ffad1b4ac793cf3d346d737c15f
hwimin-kim <gnlals7841@gmail.com>
2022-07-19 11:00:47 +0900

    event #finish

--- 2.component/ex03/src/05/App.js
+++ 2.component/ex03/src/05/App.js
@@ -2,17 +2,19 @@ import React, { useRef } from 'react';
 import './assets/scss/App.scss';
 
 export default function App() {
-    const outterRef = useRef(null);
+    const outerRef = useRef(null);
     const innerRef = useRef(null);
 
     return (
         <div
-            className={'App'}>
+            className={'App'}
+            ref={ outerRef }
+            onScroll={e => console.log(outerRef.current.clientHeight, outerRef.current.scrollTop, innerRef.current.clientHeight)} >
             <div
                 ref={ innerRef }>
                 <ul>
                     { Array.from({length: 100}, (_, i) => i+1).map(i =>
-                        <li>
+                        <li key={i}>
                             { `아이템 ${i} 입니다.` }
                         </li>
                     ) }
