d40b66566145bf54db95a71f893c342b1022f85b
hwimin-kim <gnlals7841@gmail.com>
2022-07-19 12:21:54 +0900

    Dosc: readme.md 수정

--- 2.component/ex04/src/02/index.js
+++ 2.component/ex04/src/02/index.js
@@ -1,5 +1,6 @@
-import React from "react"
-import { render } from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import Form from './Form';
 
-render(<Form />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Form/>);
