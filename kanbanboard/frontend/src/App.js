caf66fe3c6748a5865020bb67769f3cfdc6249c6
hwimin-kim <gnlals7841@gmail.com>
2022-07-20 10:03:16 +0900

    kanbanboard init

---
+++ kanbanboard/frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import cards from './assets/json/data.json';
+
+function App() {
+  console.log(cards);
+
+  return (
+    <div id={'App'}>
+      Kanbanboard
+    </div>
+  );
+}
+
+export default App
