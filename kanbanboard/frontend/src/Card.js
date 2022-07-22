18bed7f4c26da984bb4c0954c4452abfe96f3965
hwimin-kim <gnlals7841@gmail.com>
2022-07-22 12:08:37 +0900

    kanbanboard 수정

--- kanbanboard/frontend/src/Card.js
+++ kanbanboard/frontend/src/Card.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './assets/scss/Card.scss';
 import TaskList from './TaskList.js';
 
 function Card({card}) {
   const {title, description, tasks} = card;
 
+  const [showDetail, setShowDetail] = useState(true);
+
+
   return (
     <div className={styles.Card}>
-      <div className={styles.Card__Title}>{title}</div>
-      <div className={styles.Card__Title__open}>
+      <div 
+        className={showDetail ? styles.Card__Title : [styles.Card__Title, styles.Card__Title__open].join(" ")}
+        onClick={() => setShowDetail(prevShowDetail => !prevShowDetail)} >
+        {title}
+      </div>
+      {
+      showDetail ? 
+      null :       
+      <div className={'Card__Details'}>
         {description}
         <TaskList tasks={tasks}/>
       </div>
+      }
+
     </div>
   );
 }
