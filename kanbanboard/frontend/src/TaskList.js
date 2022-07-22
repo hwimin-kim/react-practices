a10c5b04c9cf4f00edd5580634074b09bd23f5aa
hwimin-kim <gnlals7841@gmail.com>
2022-07-22 12:14:31 +0900

    kanbanboard 수정 #2

--- kanbanboard/frontend/src/TaskList.js
+++ kanbanboard/frontend/src/TaskList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Task from './Task.js';
+import styles from './assets/scss/TaskList.scss';
 
 function TaskList({tasks}) {
 
@@ -8,6 +9,10 @@ function TaskList({tasks}) {
       <ul>
         {tasks.map( task => <Task key={task.no} task={task}/>)}
       </ul>
+      <input
+        type='text'
+        className={styles.TaskList__add_task}
+        placeholder='태스크 추가'/>
     </div>
   );
 }
