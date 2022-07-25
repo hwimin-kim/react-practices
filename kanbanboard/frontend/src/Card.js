d66c4543d6f61a616e8235fb980731ce6be748be
hwimin-kim <gnlals7841@gmail.com>
2022-07-25 10:10:48 +0900

    kanban 과제 풀이

--- kanbanboard/frontend/src/Card.js
+++ kanbanboard/frontend/src/Card.js
@@ -3,13 +3,22 @@ import styles from './assets/scss/Card.scss';
 import TaskList from './TaskList.js';
 
 function Card({card}) {
-  const {title, description, tasks} = card;
+  const {title, status, description, tasks} = card;
 
   const [showDetail, setShowDetail] = useState(true);
 
 
   return (
     <div className={styles.Card}>
+      <div style={{
+        position: 'absolute',
+        zIndex: -1,
+        top: 0,
+        left: 0,
+        height: '100%',
+        width: 3,
+        backgroundColor: status === 'ToDo' ? 'skyblue' : (status === 'Doing' ? '#3a7e28' : 'hotpink')
+      }}/>
       <div 
         className={showDetail ? styles.Card__Title : [styles.Card__Title, styles.Card__Title__open].join(" ")}
         onClick={() => setShowDetail(prevShowDetail => !prevShowDetail)} >
